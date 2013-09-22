78a83c0e9e1cd5c46cb95b6d19c1fe901a35dbd9
Henock Abebe <habebe@objectivity.com>
2013-09-22 13:10:31 -0700

    fresh 3 agent run.

---
+++ wbnp.agents/agent.size.3/generate.sh
@@ -0,0 +1,12 @@
+cd /data/ig.tools/run_log/wbnp.agents/agent.size.3/agent.0.initial
+python ../../../parse_wbnp_agent_log.py /data14/ig.log/dev-analysis-nosql-01.vega.ironport.com.agent.0 > overview.txt
+python ../../../generate_average.py
+
+
+cd /data/ig.tools/run_log/wbnp.agents/agent.size.3/agent.1.initial
+python ../../../parse_wbnp_agent_log.py /data14/ig.log/dev-analysis-nosql-01.vega.ironport.com.agent.1 > overview.txt
+python ../../../generate_average.py
+
+cd /data/ig.tools/run_log/wbnp.agents/agent.size.3/agent.2.initial
+python ../../../parse_wbnp_agent_log.py /data14/ig.log/dev-analysis-nosql-01.vega.ironport.com.agent.2 > overview.txt
+python ../../../generate_average.py
