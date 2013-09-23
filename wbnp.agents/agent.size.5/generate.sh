9ca6b7ad23b49339c0fdcace0feab0956bb71f18
Henock Abebe <habebe@objectivity.com>
2013-09-23 10:33:34 -0700

    5 agent run.

---
+++ wbnp.agents/agent.size.5/generate.sh
@@ -0,0 +1,24 @@
+cd /data/ig.tools/run_log/wbnp.agents/agent.size.5/agent.0.initial
+python ../../../parse_wbnp_agent_log.py /data14/ig.log/dev-analysis-nosql-01.vega.ironport.com.agent.0 > overview.txt
+python ../../../generate_average.py
+python ../../../get_disk_usage.py > disk_usage
+
+cd /data/ig.tools/run_log/wbnp.agents/agent.size.5/agent.1.initial
+python ../../../parse_wbnp_agent_log.py /data14/ig.log/dev-analysis-nosql-01.vega.ironport.com.agent.1 > overview.txt
+python ../../../generate_average.py
+python ../../../get_disk_usage.py > disk_usage
+
+cd /data/ig.tools/run_log/wbnp.agents/agent.size.5/agent.2.initial
+python ../../../parse_wbnp_agent_log.py /data14/ig.log/dev-analysis-nosql-01.vega.ironport.com.agent.2 > overview.txt
+python ../../../generate_average.py
+python ../../../get_disk_usage.py > disk_usage
+
+cd /data/ig.tools/run_log/wbnp.agents/agent.size.5/agent.3.initial
+python ../../../parse_wbnp_agent_log.py /data14/ig.log/dev-analysis-nosql-01.vega.ironport.com.agent.3 > overview.txt
+python ../../../generate_average.py
+python ../../../get_disk_usage.py > disk_usage
+
+cd /data/ig.tools/run_log/wbnp.agents/agent.size.5/agent.4.initial
+python ../../../parse_wbnp_agent_log.py /data14/ig.log/dev-analysis-nosql-01.vega.ironport.com.agent.4 > overview.txt
+python ../../../generate_average.py
+python ../../../get_disk_usage.py > disk_usage
