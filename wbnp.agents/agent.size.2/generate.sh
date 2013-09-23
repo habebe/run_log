754f8fc71f441d2f7d6b454cbd06b59c07004116
Henock Abebe <habebe@objectivity.com>
2013-09-22 20:59:32 -0700

    2 agent complete run.

--- wbnp.agents/agent.size.2/generate.sh
+++ wbnp.agents/agent.size.2/generate.sh
@@ -1,7 +1,9 @@
 cd /data/ig.tools/run_log/wbnp.agents/agent.size.2/agent.0.initial
 python ../../../parse_wbnp_agent_log.py /data14/ig.log/dev-analysis-nosql-01.vega.ironport.com.agent.0 > overview.txt
 python ../../../generate_average.py
+python ../../../get_disk_usage.py > disk_usage
 
 cd /data/ig.tools/run_log/wbnp.agents/agent.size.2/agent.1.initial
 python ../../../parse_wbnp_agent_log.py /data14/ig.log/dev-analysis-nosql-01.vega.ironport.com.agent.1 > overview.txt
 python ../../../generate_average.py
+python ../../../get_disk_usage.py > disk_usage
