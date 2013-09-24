3e786a0bd480533dfa7e74d053f3c55a62bcb346
Henock Abebe <habebe@objectivity.com>
2013-09-24 15:44:41 -0700

    2 agent run.

---
+++ whois.agents/agent.size.2/generate.sh
@@ -0,0 +1,9 @@
+cd /data/ig.tools/run_log/whois.agents/agent.size.2/agent.0.initial
+python ../../../get_disk_usage.py > disk_usage
+python ../../../parse_whois_agent_log.py /data14/ig.log/dev-analysis-nosql-01.vega.ironport.com.agent.0 > overview.txt
+python ../../../generate_average.py
+
+cd /data/ig.tools/run_log/whois.agents/agent.size.2/agent.1.initial
+python ../../../get_disk_usage.py > disk_usage
+python ../../../parse_whois_agent_log.py /data14/ig.log/dev-analysis-nosql-01.vega.ironport.com.agent.1 > overview.txt
+python ../../../generate_average.py
