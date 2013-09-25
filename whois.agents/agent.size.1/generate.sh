7dcf6a1c8b174cc6cbecda41fd741031964877b3
Henock Abebe <henocka@Henock-Abebes-MacBook-Pro.local>
2013-09-25 11:07:55 -0500

    3 agent run plots.

--- whois.agents/agent.size.1/generate.sh
+++
@@ -1,5 +0,0 @@
-cd /data/ig.tools/run_log/whois.agents/agent.size.1/agent.0.initial
-python ../../../get_disk_usage.py > disk_usage
-python ../../../parse_whois_agent_log.py /data14/ig.log/dev-analysis-nosql-01.vega.ironport.com.agent.0 > overview.txt
-python ../../../generate_average.py
-
